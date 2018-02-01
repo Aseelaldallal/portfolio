22d059ad7210ab1b456b1b249da2570104ed9698
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-02-01 10:39:22 -0500

    add image component to display project images

---
+++ src/components/UI/Image/Image.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Auxillary from '../../../hoc/Auxillary/Auxillary';
+import burger from '../../../assets/images/projectImages/burger.PNG';
+
+const Image = props => {
+  let image = null;
+
+  switch (props.image) {
+    case 'burger':
+      image = <img src={burger} alt="buildaburger" />;
+      break;
+    default:
+      image = null;
+  }
+
+  return <Auxillary>{image}</Auxillary>;
+};
+
+export default Image;
