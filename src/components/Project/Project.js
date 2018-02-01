4bbe1cb9dc7bf5d42295da14e5a028076eb6975b
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-02-01 11:08:06 -0500

    Add buttons to project component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import classes from './Project.css';
+import Auxillary from '../../hoc/Auxillary/Auxillary';
+import Button from '../UI/Button/Button';
 import Image from '../UI/Image/Image';
 
 const Project = props => {
@@ -10,20 +12,27 @@ const Project = props => {
   }
 
   return (
-    <div className={projectStyle.join(' ')}>
-      <div className={classes.imgContainer}>
-        <Image image={props.image} />
+    <Auxillary>
+      <div className={projectStyle.join(' ')}>
+        <div className={classes.imgContainer}>
+          <Image image={props.image} />
+        </div>
+        <div className={classes.spacer} />
+        <div className={classes.projectDesc}>
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          <p className={classes.tech}>
+            <strong>Tech: </strong>
+            {props.tech}
+          </p>
+        </div>
       </div>
-      <div className={classes.spacer} />
-      <div className={classes.projectDesc}>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-        <p className={classes.tech}>
-          <strong>Tech: </strong>
-          {props.tech}
-        </p>
+      <div className={classes.buttonContainer}>
+        <Button label="visit" />
+
+        <Button label="github" />
       </div>
-    </div>
+    </Auxillary>
   );
 };
 
