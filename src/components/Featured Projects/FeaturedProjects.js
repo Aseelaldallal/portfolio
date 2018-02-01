ba306cfc3330b9dfbec5a927c987ef4f6ad644b6
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-02-01 10:39:50 -0500

    adjust styling

--- src/components/Featured Projects/FeaturedProjects.js
+++ src/components/Featured Projects/FeaturedProjects.js
@@ -1,6 +1,12 @@
+// React
 import React from 'react';
-import classes from './FeaturedProjects.css';
+// Components
 import Title from '../UI/Title/Title';
+import Project from '../Project/Project';
+// Images
+import burger from '../../assets/images/projectImages/burger.PNG';
+// Style
+import classes from './FeaturedProjects.css';
 
 const FeaturedProjects = () => {
   return (
@@ -8,6 +14,13 @@ const FeaturedProjects = () => {
       <Title
         title="Featured Projects"
         subheading="Psst.. Checkout the github links for notes on how each app was developed."
+      />
+      <Project
+        alignPicRight
+        title="Build A Burger"
+        description="Web App that allows registered users to build a burger and purchase it. Go ahead. Test it out!"
+        tech="React, Redux, Firebase"
+        image="burger"
       />
     </section>
   );
---
+++ src/components/Project/Project.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Project.css';
+import Image from '../UI/Image/Image';
+// import burger from '../../assets/images/projectImages/burger.PNG';
+
+const Project = props => {
+  let projectStyle = [];
+  projectStyle.push(classes.project);
+  if (props.alignPicRight) {
+    projectStyle.push(classes.reverse);
+  }
+
+  return (
+    <div className={projectStyle.join(' ')}>
+      <Image image={props.image} />
+
+      <div className={classes.projectDesc}>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        <p className={classes.tech}>
+          <strong>Tech: </strong>
+          {props.tech}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
