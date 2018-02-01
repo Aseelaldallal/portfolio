8909743ce98d83db517dadc598f9a2e800433402
unknown <aseelaldallal@gmail.com>
2018-01-31 21:16:22 -0500

    create home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './components/Navigation/NavBar/NavBar';
+import Home from './components/Home/Home';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div><NavBar />
+      <div>
+        <NavBar />
+        <Home />
       </div>
     );
   }
