9a162bb61c603e8fcab5823af7cdd6276075bc3b
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-02-08 12:17:54 -0500

    make navbar sticky

--- src/components/Navigation/MenuItem/MenuItem.js
+++ src/components/Navigation/MenuItem/MenuItem.js
@@ -2,17 +2,16 @@ import React from 'react';
 import classes from './MenuItem.css';
 
 const MenuItem = props => {
-
-    let itemStyles = [classes.menuItem];
-    if (props.selected) {
-        itemStyles.push(classes.selectedItem);
-    }
-    console.log(itemStyles.join(' '));
-    return (
-        <a href="#" className={itemStyles.join(' ')}>
-            {props.name}
-        </a>
-    );
-}
+  let itemStyles = [classes.menuItem];
+  if (props.selected) {
+    itemStyles.push(classes.selectedItem);
+  }
+  console.log(itemStyles.join(' '));
+  return (
+    <a href={props.link ? props.link : '#'} className={itemStyles.join(' ')}>
+      {props.name}
+    </a>
+  );
+};
 
 export default MenuItem;
--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -2,26 +2,20 @@ import React from 'react';
 import classes from './NavBar.css';
 import MenuItem from '../MenuItem/MenuItem';
 
-
 const NavBar = () => {
+  function clicked() {}
 
-
-    function clicked() {
-        
-    }
-
-    return (
-        <div className={classes.NavBar}>
-            <div className={classes.MenuItemList}>
-                <MenuItem name="Home" selected />
-                <MenuItem name="Skills" />
-                <MenuItem name="Projects" />
-                <MenuItem name="Resume" />
-                <MenuItem name="Contact" />
-            </div>
-        </div>
-    );
-
-}
+  return (
+    <div className={classes.NavBar}>
+      <div className={classes.MenuItemList}>
+        <MenuItem name="Home" selected />
+        <MenuItem name="Skills" link="#skills" />
+        <MenuItem name="Projects" />
+        <MenuItem name="Resume" />
+        <MenuItem name="Contact" />
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
